a4d9db9c5b7ef62984201165372bf7ae842be848
Martha-KW <spielsachentests@gmail.com>
2025-05-01 20:16:21 +0200

    Flask instance added

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-print(1234)
+# Create an instance of the Flask application
+app = Flask(__name__)
+
+# Define the route for the homepage
+@app.route('/')
+def home():
+    return "Welcome to the Home Page!"
+
+# If you want to run the app
+if __name__ == '__main__':
+    app.run(debug=True)
