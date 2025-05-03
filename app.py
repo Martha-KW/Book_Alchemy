cc1291d868b3bb365d5a413de0e965cf28072b7d
Martha-KW <spielsachentests@gmail.com>
2025-05-03 10:07:46 +0200

    first version of app.py inserted, empty data_models.py added

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from data_models import db, Author, Book
+
 
 # Create an instance of the Flask application
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 # Define the route for the homepage
 @app.route('/')
